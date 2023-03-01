1aa0d87063e79c1124aa8b052b3e631c9243ceaf
Alfre <alfreddhcp@outlook.com>
2023-03-01 20:26:55 +0000

    V1.2

--- Challenge1.java
+++ Challenge1.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Write a description of class Challenge1 here.
@@ -8,8 +9,32 @@ import java.util.ArrayList;
  */
 public class Challenge1
 {
+    private static ArrayList<String> botMemoryUsages;
+
     public static void main(String[] args) {
+        sortBotUsages();
+        System.out.println(generateAnswerAsString(5));
+    }
+
+    /**
+     * main calculations goes here
+     */
+    private static void sortBotUsages() {
+        int currentBotUsage = 0;
+        botMemoryUsages = new ArrayList<>();
         ArrayList<String> data = FileReader.readFile("challenge_1.txt");
-        System.out.println(data);
+        //read numbers from lines
+        for(String line: data) {
+            if(line.isBlank()) {
+                botMemoryUsages.add(Integer.toString(currentBotUsage));
+                currentBotUsage = 0;
+            }
+            else {
+                currentBotUsage += Integer.parseInt(line);
+            }
+        }
+
+        Collections.sort(botMemoryUsages);
+        Collections.reverse(botMemoryUsages);
     }
 }
